66b9b1af1da355f964c630312a572e80250af0c5
Leonard Mbelle <58833407+lenadlm@users.noreply.github.com>
2025-03-31 07:42:22 +0300

    Update docker.sh
    Signed-off-by: Leonard Mbelle <58833407+lenadlm@users.noreply.github.com>

--- srv-1/tmp/docker.sh
+++ srv-1/tmp/docker.sh
@@ -99,7 +99,7 @@ else
 fi
 
 # Define variables for deployment
-COMPOSE_FILE_URL="https://raw.githubusercontent.com/lenadlm/docker/main/portainer/docker-compose.yml"
+COMPOSE_FILE_URL="https://raw.githubusercontent.com/lenadlm/docker/main/srv-1/portainer/docker-compose.yml"
 COMPOSE_DIR="/opt/docker/portainer"
 
 # Check if the URL is accessible
